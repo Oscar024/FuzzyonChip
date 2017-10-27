2a3cf2a7058daf4a5e20ec3f0f88805e014eb6ee
Oscar024 <or.carvajal.ortiz@gmail.com>
2017-10-26 18:14:11 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Archivo: main.c
+ * Autores: Ing. Oscar Rigoberto Carvajal Ortiz
+ * Fecha: 28 de Osctubre de 2016, 07:03 PM
+ * Versión: 1.0
+ * IDE: MPLAB XC8
+ * Empresa: Maestría en Ciencias de la Computación ITT
+ * MCU: PIC18F4550
+
+ ******************************************************************************/
+
+
+#include <xc.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "pic18f4550config.h"
+#include <pic18f4550.h>
+#include "fuzzy.h"
+#include "pwm.h"
+#include "cny70.h"
+#include "motor.h"
+#include "funciones.h"
+
+int main()
+{
+
+  mcu_init();
+  PORTAbits.RA0 = 1;
+  
+  
+  while(1){
+      double a, b;
+      int sensed;
+      
+
+      sensed = getCNY70Value();
+      a = fuzzy_system_single1(sensed);
+      b = fuzzy_system_single2(sensed);
+      
+      if(SL2 == 1  && SL1 == 1 && SM == 1 && SR1 == 1 && SR2 == 1){
+            go_back(100,100);
+       }       
+      else{
+          motor_forward();
+          PWM_DutyCycle1(a);
+          PWM_DutyCycle1(b);
+      }
+       
+      
+      
+  }
+  
+  return 0;
+}
+ 
+
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FuzzyMCUlineFollower-master.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FuzzyMCUlineFollower-master.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=fuzzymculinefollower-master.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/fuzzymculinefollower-master.tar
